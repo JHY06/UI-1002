1a5e450a80174e326f4ea05987a0ed63778467dd
JHY06 <ouou1326@gmail.com>
2024-10-07 12:03:41 +0900

    구현

--- 03-tabmenu/js/script.js
+++ 03-tabmenu/js/script.js
@@ -6,11 +6,14 @@ $(function () {
     $tabcon.eq(0).show;
 });
 $tabmenu.on("click", function (e) {
-    e.preventDefault();
-    const tabIdx = $(this.index());
     console.log(tabIdx);
-    $tabmenu.removeClass("on");
-    $tabmenu.on(tabIdx).addClass("on");
-    $tabcon.hide();
-    $tabcon.eq(tabIdx).show();
+    function tabAciton() {
+        e.preventDefault();
+        const tabIdx = $(this.index());
+        console.log(tabIdx);
+        $tabmenu.removeClass("on");
+        $tabmenu.on(tabIdx).addClass("on");
+        $tabcon.hide();
+        $tabcon.eq(tabIdx).show();
+    }
 });
